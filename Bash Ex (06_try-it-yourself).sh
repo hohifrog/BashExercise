0f71acb29b2905e501744044597ec024c63d3b96
hohifrog <hohironyanai@gmail.com>
2023-01-27 09:19:06 +0200

    Improving if services is running loop

--- Bash Ex (06_try-it-yourself).sh
+++ Bash Ex (06_try-it-yourself).sh
@@ -20,12 +20,22 @@ echo $RCSuccess
 fi
 #-------------------------------------
 #Services level 3
-ls /etc/rc3.d/S*
-if [[ $? -ne 0 ]] ; then
-RCSuccess=$(($RCSuccess + $RCService))
-echo $RCSuccess
-fi
-#-------------------------------------
+services=$(ls /etc/rc3.d/S*)
+
+for service in $services; do
+  service_name=$(echo $service | awk -F '/' '{print $NF}')
+  service_status=$(systemctl is-active $service_name)
+  if [[ $service_status == "active" ]]; then
+    echo "$service_name is running."
+  else
+    echo "$service_name is not running."
+    RCSuccess=$(($RCSuccess + $RCService))
+  fi
+done
+
 #Final Return Code
 if [[ $RCSuccess -eq 0 ]] ; then
 echo "Script executed without errors, Exit code: $RCSuccess"
+else
+echo "Script executed with errors, Exit code: $RCSuccess"
+fi
